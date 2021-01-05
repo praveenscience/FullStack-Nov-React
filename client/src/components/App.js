9b507684ee9c3673ff27d30ef39016cb6ff7601e
Praveen Kumar <praveenscience@gmail.com>
2021-01-05 06:29:32 +0000

    Reorganised file structure & cleaned up from CRA.

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Learn React!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
