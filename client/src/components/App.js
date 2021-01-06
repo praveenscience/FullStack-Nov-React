aa862a07ed3b2325b1c45bab8f88066061abd493
Praveen Kumar <praveenscience@gmail.com>
2021-01-06 06:18:57 +0000

    Added the contents of state and displayed it on a pre tag.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    Name: "Praveen",
+    Age: 27,
+    Address: {
+      City: "London",
+      State: "England",
+      Country: "UK"
+    },
+    Phones: ["+449876543210", "+919876543210"],
+    Professional: true,
+    DateOfReg: new Date()
+  };
   render() {
     return (
       <div className="App">
-        <p>Learn React!</p>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 py-3">
+              <p>Hello {this.state.Name}</p>
+              <pre className="my-3 p-2 bg-light border rounded">
+                {JSON.stringify(this.state, null, 2)}
+              </pre>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
