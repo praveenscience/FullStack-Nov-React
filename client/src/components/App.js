a02bfec22d145e7c71ac7675312e52773b635deb
Praveen Kumar <praveenscience@gmail.com>
2021-01-06 06:37:04 +0000

    Displaying a counter and show a text when complete!

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class App extends Component {
   state = {
-    Name: "Praveen",
+    Name: "Praveen Kumar",
     Age: 27,
     Address: {
       City: "London",
@@ -11,8 +11,23 @@ class App extends Component {
     },
     Phones: ["+449876543210", "+919876543210"],
     Professional: true,
-    DateOfReg: new Date()
+    DateOfReg: new Date(),
+    Count: 10
   };
+  componentDidMount() {
+    const timerId = setInterval(() => {
+      if (this.state.Count - 1 === 0) {
+        clearInterval(timerId);
+        this.setState({
+          Count: "Completed!"
+        });
+      } else {
+        this.setState({
+          Count: this.state.Count - 1
+        });
+      }
+    }, 1000);
+  }
   render() {
     return (
       <div className="App">
@@ -20,6 +35,11 @@ class App extends Component {
           <div className="row">
             <div className="col-12 py-3">
               <p>Hello {this.state.Name}</p>
+              <p>
+                Countdown to something! You have{" "}
+                {this.state.Count.toString().toLowerCase() +
+                  (this.state.Count !== "Completed!" ? " seconds left!" : "")}
+              </p>
               <pre className="my-3 p-2 bg-light border rounded">
                 {JSON.stringify(this.state, null, 2)}
               </pre>
