ac1c0c30399ccff0565d419bfd5f89a99c118e7a
Praveen Kumar <praveenscience@gmail.com>
2021-01-07 06:35:42 +0000

    Implemented a Card Component for each animal.

--- client/src/components/Animal.js
+++ client/src/components/Animal.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Card from "./Card";
 
-const Animal = ({ children }) => {
-  return <div className="Animal p-3 bg-primary col-3">{children}</div>;
+const Animal = ({ Image, Name }) => {
+  return (
+    <div className="Animal py-3 col-4">
+      <Card Image={Image} ImgAlign="top" Title={Name}>
+        <button className="btn btn-sm btn-outline-primary">Like</button>
+      </Card>
+    </div>
+  );
 };
 
 export default Animal;
