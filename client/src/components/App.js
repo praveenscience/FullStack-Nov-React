57ddf9137c1f4161878962376442156b66cd35a5
Praveen Kumar <praveenscience@gmail.com>
2021-01-07 05:39:38 +0000

    Cleanup of countdown and states.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,32 +2,9 @@ import React, { Component } from "react";
 import Praveen from "./Header";
 
 class App extends Component {
-  state = {
-    Name: "Praveen Kumar",
-    Age: 27,
-    Address: {
-      City: "London",
-      State: "England",
-      Country: "UK"
-    },
-    Phones: ["+449876543210", "+919876543210"],
-    Professional: true,
-    DateOfReg: new Date(),
-    Count: 10
-  };
+  state = {};
   componentDidMount() {
-    const timerId = setInterval(() => {
-      if (this.state.Count - 1 === 0) {
-        clearInterval(timerId);
-        this.setState({
-          Count: "Completed!"
-        });
-      } else {
-        this.setState({
-          Count: this.state.Count - 1
-        });
-      }
-    }, 1000);
+    // Let's watch it later.
   }
   render() {
     return (
@@ -37,17 +14,7 @@ class App extends Component {
         </Praveen>
         <div className="container">
           <div className="row">
-            <div className="col-12 py-3">
-              <p>Hello {this.state.Name}</p>
-              <p>
-                Countdown to something! You have{" "}
-                {this.state.Count.toString().toLowerCase() +
-                  (this.state.Count !== "Completed!" ? " seconds left!" : "")}
-              </p>
-              <pre className="my-3 p-2 bg-light border rounded">
-                {JSON.stringify(this.state, null, 2)}
-              </pre>
-            </div>
+            <div className="col-12 text-center">Main application.</div>
           </div>
         </div>
       </div>
