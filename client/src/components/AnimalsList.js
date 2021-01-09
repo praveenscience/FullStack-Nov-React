86d9a9c76a4a47d744f78d66103fbe2fa694dfe3
Praveen Kumar <praveenscience@gmail.com>
2021-01-09 05:46:16 +0000

    Display more actions using short-circuited &.
    * Show the button list for Liked, (Don't) Want to See conditionally.
    * The buttons should be shown only when at least one animal is liked.

--- client/src/components/AnimalsList.js
+++ client/src/components/AnimalsList.js
@@ -21,6 +21,24 @@ const AnimalsList = ({ List }) => {
       : animal.Name.toLowerCase().indexOf(Query.toLowerCase()) > -1;
   return (
     <div className="Animals">
+      {Liked.length > 0 && (
+        <div className="row">
+          <div className="col-12 text-center pt-3">
+            <button className="btn btn-outline-primary btn-sm mr-2">
+              Home
+            </button>
+            <button className="btn btn-outline-primary btn-sm mr-2">
+              Liked Animals
+            </button>
+            <button className="btn btn-outline-primary btn-sm mr-2">
+              Want to See
+            </button>
+            <button className="btn btn-outline-primary btn-sm">
+              Don't Want to See
+            </button>
+          </div>
+        </div>
+      )}
       <Search Query={Query} setQuery={setQuery} />
       <div className="row my-3">
         {[...List].length > 0
