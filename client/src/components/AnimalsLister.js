3eca2f5480d265dfa6c81217042896644a55ab8f
Praveen Kumar <praveenscience@gmail.com>
2021-01-09 06:48:21 +0000

    Implemented an empty state and a link to go home.

--- client/src/components/AnimalsLister.js
+++ client/src/components/AnimalsLister.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Animal from "./Animal";
 
 const AnimalsLister = ({ List, Query, LikeHandler, Liked }) => {
@@ -6,19 +7,27 @@ const AnimalsLister = ({ List, Query, LikeHandler, Liked }) => {
     Query.length === 0
       ? true
       : animal.Name.toLowerCase().indexOf(Query.toLowerCase()) > -1;
-  return [...List].length > 0
-    ? [...List]
-        .filter(AnimalsFilter)
-        .map(animal => (
-          <Animal
-            key={animal.Name}
-            Image={animal.Image}
-            Name={animal.Name}
-            LikeHandler={LikeHandler}
-            Liked={Liked.includes(animal.Name)}
-          />
-        ))
-    : "Nothing Found";
+  return [...List].length > 0 ? (
+    [...List]
+      .filter(AnimalsFilter)
+      .map(animal => (
+        <Animal
+          key={animal.Name}
+          Image={animal.Image}
+          Name={animal.Name}
+          LikeHandler={LikeHandler}
+          Liked={Liked.includes(animal.Name)}
+        />
+      ))
+  ) : (
+    <div className="col-12">
+      <div className="alert alert-danger">
+        <p className="m-0 text-center">
+          Nothing found! Head back to <Link to="/">Home</Link>.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default AnimalsLister;
