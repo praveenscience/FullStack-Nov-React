3293114984112d4162db1e22b107556128225510
Praveen Kumar <praveenscience@gmail.com>
2021-01-11 06:51:27 +0000

    Created Categories page and added one AnimalsList.

---
+++ client/src/components/CategoriesPage.js
@@ -0,0 +1,42 @@
+import React from "react";
+import AnimalsLister from "./AnimalsLister";
+import Card from "./Card";
+
+const CategoriesPage = ({
+  Query,
+  List,
+  LikeHandler,
+  Liked,
+  See,
+  SeeHandler
+}) => {
+  return (
+    <div className="col-12">
+      <div className="CategoriesPage">
+        <Card Header="Uncategorised" className="my-3">
+          <div className="row">
+            <AnimalsLister
+              Query={Query}
+              List={List}
+              LikeHandler={LikeHandler}
+              Liked={Liked}
+              See={See}
+              SeeHandler={SeeHandler}
+            />
+          </div>
+        </Card>
+        <Card Header="Herbivorous" className="my-3">
+          Hi
+        </Card>
+        <Card Header="Carnivorous" className="my-3">
+          Hi
+        </Card>
+        <Card Header="Omnivorous" className="my-3">
+          Hi
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default CategoriesPage;
