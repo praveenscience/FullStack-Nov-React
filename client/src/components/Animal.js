578d6a750d51abc1d6b085034e64a26a7ab50746
Praveen Kumar <praveenscience@gmail.com>
2021-01-11 05:58:13 +0000

    Displayed the See button solid or outline based on Want to See status.

--- client/src/components/Animal.js
+++ client/src/components/Animal.js
@@ -6,6 +6,10 @@ const Animal = ({ Image, Name, LikeHandler, Liked, Saw, SeeHandler }) => {
     e.preventDefault();
     LikeHandler(Name);
   };
+  const handleSee = e => {
+    e.preventDefault();
+    SeeHandler(Name);
+  };
   return (
     <div className="Animals-Item py-3 col-4">
       <Card Image={Image} ImgAlign="top" Title={Name}>
@@ -17,7 +21,12 @@ const Animal = ({ Image, Name, LikeHandler, Liked, Saw, SeeHandler }) => {
           ike
         </button>
         {Liked && (
-          <button className="btn btn-sm btn-outline-primary mr-1">
+          <button
+            className={
+              "btn btn-sm btn" + (Saw ? "" : "-outline") + "-primary mr-1"
+            }
+            onClick={handleSee}
+          >
             <i className="icon-eye"></i>
           </button>
         )}
