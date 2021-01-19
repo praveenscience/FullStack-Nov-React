361bafb3126d437199ead8c5197ec38e1a4a332f
Praveen Kumar <praveenscience@gmail.com>
2021-01-19 06:08:04 +0000

    Showed Animals Endpoint with JSON file contents.
    - Created an Animals endpoint inside API.
    - Read a JSON file and show in an dndpoint.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express.Router();
+const animals = require("../constants/animals.json");
 
 app.get("/", (req, res) => {
   res.json("Hello API!");
+});
+app.get("/animals", (req, res) => {
+  res.json(animals);
 });
 
 module.exports = app;
