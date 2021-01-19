3330fee68c13475d659764fea741b32cf62be5e2
Praveen Kumar <praveenscience@gmail.com>
2021-01-19 06:03:33 +0000

    Created a Modular Routing using Express JS.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+// Importing routes.
+const root = require("./routes/root");
 
-app.get("/", (req, res) => {
-  res.json("Hello World!");
-});
+app.use("/", root);
 
 app.listen(port, () => {
   console.log(`Server started in port ${port}.`);
