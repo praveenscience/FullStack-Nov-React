96304b4f8a3035291e5c2d03c87129539e01ab68
Praveen Kumar <praveenscience@gmail.com>
2021-01-19 06:05:28 +0000

    Created a API route using Modular Routing.

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3000;
 // Importing routes.
 const root = require("./routes/root");
+const api = require("./routes/api");
 
 app.use("/", root);
+app.use("/api", api);
 
 app.listen(port, () => {
   console.log(`Server started in port ${port}.`);
