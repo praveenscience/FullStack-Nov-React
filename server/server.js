f84a5879dd14860cb5a1a45483cf57fa86abcaf7
Praveen Kumar <praveenscience@gmail.com>
2021-01-20 05:43:50 +0000

    Added morgan middleware.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3100;
 // Importing routes.
 const root = require("./routes/root");
 const api = require("./routes/api");
 
+// Middlewares
+app.use(morgan("dev"));
+
+// Routes
 app.use("/", root);
 app.use("/api", api);
 
