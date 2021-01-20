9c85014fa61afece072f779fc057b1794474b371
Praveen Kumar <praveenscience@gmail.com>
2021-01-20 06:18:51 +0000

    Updated the terminology for Delete Animal.

--- client/src/services/Animals.js
+++ client/src/services/Animals.js
@@ -2,4 +2,5 @@ import Axios from "axios";
 
 export const GetAllAnimals = () => Axios.get("/api/animals");
 
-export const DeleteAnimal = animal => Axios.delete("/api/animals/" + animal);
+export const DeleteAnimal = AnimalId =>
+  Axios.delete("/api/animals/" + AnimalId);
